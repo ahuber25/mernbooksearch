f847441cf8ce64dc774039b3a6bd21f9afa75385
Scotch <horsinaround235@gmail.com>
2022-10-15 08:31:33 -0400

    added apollo server to server

--- server/server.js
+++ server/server.js
@@ -2,12 +2,29 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  authMiddleware
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const startApolloServer = async () => {
+  await server.start()
+  server.applyMiddleware({ app })
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+ startApolloServer();
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
